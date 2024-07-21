75cc4f72b3e5030a081850856a31530bbdfebba4
Nathan Cazard <cazard.nathan@gmail.com>
2024-07-21 15:13:58 +0200

    limiter les objets à retirer de 1 à 6 & ajouter la notion de victoire

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
-var nbObj = 50;
-var divObj;
+var nbObj = 5;
+var divObj = document.getElementById('nbObjs');
 var displayObjs = function () {
-    divObj = document.getElementById("nbObjs");
-    if (divObj)
-        divObj.innerText = nbObj;
-    return divObj;
-};
-displayObjs();
-var removeSomeObjs = function () {
-    //const objToRemove = document.getElementById("objToRemove").innerText as HTMLElement
-    var quantity = prompt("Combien d'allumettes retirons nous ?");
-    console.log(quantity, typeof quantity);
-    while (quantity == "" || quantity == null) {
-        quantity = prompt("Combien d'allumettes retirons nous ?");
-    }
-    var quantityToRemove = parseInt(quantity);
-    nbObj -= quantityToRemove;
     if (divObj)
         divObj.innerText = nbObj.toString();
+    return divObj;
 };
-var victoire = function (param) {
-    var win = false;
-    while (param > 0) {
-        //appeler la fonction pour enlever x objets
+/* const removeSomeObjs = () => {
+    let quantity: null | string = prompt("Combien d'allumettes retirons nous ?");
+    console.log(quantity, typeof quantity);
+    while (quantity == '' || quantity == null) {
+        quantity = prompt("Combien d'allumettes retirons nous ?");
+    }
+    let quantityToRemove: number = parseInt(quantity);
+    nbObj -= quantityToRemove;
+    if (divObj) divObj.innerText = nbObj.toString();
+    return nbObj;
+}; */
+var removeSomeObjs2 = function () {
+    var errorParagraph = document.getElementById('error');
+    errorParagraph.innerText = '';
+    var inputElement = document.getElementById('objToRemove');
+    if (inputElement) {
+        var quantity = inputElement.value;
+        var quantityToRemove = parseInt(quantity);
+        if (isNaN(quantityToRemove) || quantityToRemove < 1 || quantityToRemove > 6) {
+            var errorMessage = void 0;
+            if (isNaN(quantityToRemove)) {
+                errorMessage = "Merci d'entrer une valeur";
+            }
+            else {
+                errorMessage = "Le nombre d'objets à retirer doit être entre 1 et 6";
+            }
+            errorParagraph.innerText = errorMessage;
+            inputElement.value = '';
+            return;
+        }
+        else {
+            nbObj -= quantityToRemove;
+            if (divObj)
+                divObj.innerText = nbObj.toString();
+            inputElement.value = '';
+        }
+    }
+    return nbObj;
+};
+var displayChange = function () {
+    var victoryDiv = document.getElementById('victoire');
+    victoryDiv.innerText = 'Bravo, vous avez gagné !';
+    victoryDiv.style.display = 'flex';
+    var gameDiv = document.getElementById('gamePlayer');
+    gameDiv.style.display = 'none';
+};
+var victoire = function () {
+    removeSomeObjs2();
+    if (nbObj <= 0) {
+        if (divObj)
+            divObj.innerText = '0';
+        displayChange();
     }
 };
+displayObjs();
--- script.ts
+++ script.ts
@@ -1,30 +1,65 @@
-let nbObj: number = 50;
-let divObj;
+let nbObj: number = 5;
+let divObj = document.getElementById('nbObjs');
 
 const displayObjs = () => {
-    divObj = document.getElementById("nbObjs");
-    if (divObj) divObj.innerText = nbObj; //.toString();
+    if (divObj) divObj.innerText = nbObj.toString();
     return divObj;
 };
 
-displayObjs();
-
-const removeSomeObjs = () => {
-    //const objToRemove = document.getElementById("objToRemove").innerText as HTMLElement
+/* const removeSomeObjs = () => {
     let quantity: null | string = prompt("Combien d'allumettes retirons nous ?");
     console.log(quantity, typeof quantity);
-    while (quantity == "" || quantity == null) {
+    while (quantity == '' || quantity == null) {
         quantity = prompt("Combien d'allumettes retirons nous ?");
     }
     let quantityToRemove: number = parseInt(quantity);
     nbObj -= quantityToRemove;
     if (divObj) divObj.innerText = nbObj.toString();
     return nbObj;
+}; */
+
+const removeSomeObjs2 = () => {
+    const errorParagraph = document.getElementById('error') as HTMLParagraphElement;
+    errorParagraph.innerText = '';
+    const inputElement = document.getElementById('objToRemove') as HTMLInputElement;
+
+    if (inputElement) {
+        let quantity: string | null = inputElement.value;
+        const quantityToRemove: number = parseInt(quantity);
+
+        if (isNaN(quantityToRemove) || quantityToRemove < 1 || quantityToRemove > 6) {
+            let errorMessage: string;
+            if (isNaN(quantityToRemove)) {
+                errorMessage = "Merci d'entrer une valeur";
+            } else {
+                errorMessage = "Le nombre d'objets à retirer doit être entre 1 et 6";
+            }
+            errorParagraph.innerText = errorMessage;
+            inputElement.value = '';
+            return;
+        } else {
+            nbObj -= quantityToRemove;
+            if (divObj) divObj.innerText = nbObj.toString();
+            inputElement.value = '';
+        }
+    }
+    return nbObj;
 };
 
-const victoire = (param) => {
-    let win: boolean = false;
-    while (param > 0) {
-        //appeler la fonction pour enlever x objets
+const displayChange = () => {
+    const victoryDiv = document.getElementById('victoire') as HTMLDivElement;
+    victoryDiv.innerText = 'Bravo, vous avez gagné !';
+    victoryDiv.style.display = 'flex';
+    const gameDiv = document.getElementById('gamePlayer') as HTMLDivElement;
+    gameDiv.style.display = 'none';
+};
+
+const victoire = () => {
+    removeSomeObjs2();
+    if (nbObj <= 0) {
+        if (divObj) divObj.innerText = '0';
+        displayChange();
     }
 };
+
+displayObjs();
